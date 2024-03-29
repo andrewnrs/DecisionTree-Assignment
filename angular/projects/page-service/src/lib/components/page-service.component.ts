4457e1cb92f34eb1a68b7d27df3501cf6f7dd411
Andrew Silva <andrewnrs@outlook.com>
2024-03-29 04:13:16 -0300

    updated yarn

--- angular/projects/page-service/src/lib/components/page-service.component.ts
+++ angular/projects/page-service/src/lib/components/page-service.component.ts
@@ -1,18 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { Page, PageServiceService } from '../services/page-service.service';
 import { Observable } from 'rxjs';
+//import { Editor } from 'ngx-editor';
 
 @Component({
   selector: 'lib-page-service',
-  template: `<div style="background:#FFF000; align-items: center;  justify-content: center;  display: flex;"> 
-              <span [innerHTML]="(html | async)?.content"></span>              
-            </div>`,
+  templateUrl: './page-service.component.html',
   styles: [],
+  providers: []
 })
 export class PageServiceComponent implements OnInit {
 
   constructor(private service: PageServiceService) {}
-  
+
   teste: string = `<h6>testando</h6>`;
   page$: Observable<Page> = new Observable<Page>();
   html: Observable<Page> = new Observable<Page>();
